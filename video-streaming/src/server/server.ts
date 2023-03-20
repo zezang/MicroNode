27cc378428247fc790ffccd28c196eebfddb11cc
zezang <zezang@gmail.com>
2023-03-19 21:46:00 -0700

    updated app to include mongodb

--- video-streaming/src/server/server.ts
+++ video-streaming/src/server/server.ts
@@ -1,42 +1,59 @@
 import express, { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
 import http, { IncomingHttpHeaders } from "http";
-
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
 dotenv.config();
 
-// if (!process.env.PORT) throw new Error('Please specify the port number [CA] for the HTTP server with the environment variable PORT.');
 const PORT: number | string = process.env.PORT || 3000;
-
-const isDev = process.env.NODE_ENV === 'development';
 
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => res.send('Hello World'));
+async function main () {
+  const client = await mongodb.MongoClient.connect(DBHOST);
+  const db = client.db(DBNAME);
+  
+  const videos = db.collection('videos');
 
-app.get('/videos', async (req: Request, res: Response) => {
-    const forwardRequest = http.request({
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: '/video?path=SampleVideo_1280x720_1mb.mp4',
-      method: 'GET',
-      headers: req.headers,
-    },
-    forwardResponse => {
-      const headers = forwardResponse.headers as IncomingHttpHeaders;
-      res.writeHead(forwardResponse.statusCode, headers);
-      forwardResponse.pipe(res);
-    });
+  app.get('/', (req, res) => res.send('Hello World'));
 
-    req.pipe(forwardRequest);
-});
+  app.get('/video', async (req: Request, res: Response) => {
+      const id = Number(req.query.id);
+      const videoId = new mongodb.ObjectId(id);
+      
+      const videoRecord = await videos.findOne({ _id: videoId });
+      if (!videoRecord) return res.sendStatus(404);
+
+      const forwardRequest = http.request({
+        host: VIDEO_STORAGE_HOST,
+        port: VIDEO_STORAGE_PORT,
+        path: `/video?path=${videoRecord.videoPath}`,
+        method: 'GET',
+        headers: req.headers,
+      },
+      forwardResponse => {
+        const headers = forwardResponse.headers as IncomingHttpHeaders;
+        res.writeHead(forwardResponse.statusCode, headers);
+        forwardResponse.pipe(res);
+      });
+
+      req.pipe(forwardRequest);
+  });
+
+  app.listen(PORT, () => console.log(`Video streaming microservice connected to port ${PORT}`));
+};
+
+main()
+  .catch(err => {
+    console.log('Video streaming microservice failed to start.');
+    console.error(err && err.stack || err);
+  });
 
 
-
-app.listen(PORT, () => console.log(`Video streaming microservice connected to port ${PORT}`));

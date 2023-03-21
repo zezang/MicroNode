8bc5ebac72870fc27d7e92ac2faef971bf365adc
zezang <zezang@gmail.com>
2023-03-21 00:04:41 -0700

    working dev container

--- video-streaming/src/server/server.ts
+++ video-streaming/src/server/server.ts
@@ -60,7 +60,7 @@ async function main() {
 
   app.get("/video", async (req, res) => { // Route for streaming video.
       
-      const videoPath = "./assets/videos/SampleVideo_1280x720_1mb.mp4";
+      const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
       const stats = await fs.promises.stat(videoPath);
   
       res.writeHead(200, {

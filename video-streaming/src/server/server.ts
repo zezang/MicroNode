14e16f467e9d3bae8ede8f870827b00f35b9fcbf
zezang <zezang@gmail.com>
2023-03-20 23:56:14 -0700

    created docker compose dev file

--- video-streaming/src/server/server.ts
+++ video-streaming/src/server/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
 import http, { IncomingHttpHeaders } from "http";
 import { MongoClient, ObjectId } from 'mongodb';
+import fs from 'fs';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -16,41 +18,63 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-async function main () {
-  const client = await MongoClient.connect(DBHOST);
-  const db = client.db(DBNAME);
+// async function main () {
+//   const client = await MongoClient.connect(DBHOST);
+//   const db = client.db(DBNAME);
   
-  const videos = db.collection('videos');
+//   const videos = db.collection('videos');
 
-  app.get('/', (req, res) => res.send('Hello World'));
+//   app.get('/', (req, res) => res.send('Hello World'));
 
-  app.get('/video', async (req: Request, res: Response) => {
-      const id = req.query.id.toString();
-      const videoId = new ObjectId(id);
+//   app.get('/video', async (req: Request, res: Response) => {
+//       const id = req.query.id.toString();
+//       const videoId = new ObjectId(id);
 
-      const videoRecord = await videos.findOne({ _id: videoId });
+//       const videoRecord = await videos.findOne({ _id: videoId });
 
-      console.log('Result from query: ', videoRecord)
-      if (!videoRecord) return res.sendStatus(404);
+//       console.log('Result from query: ', videoRecord)
+//       if (!videoRecord) return res.sendStatus(404);
 
-      const forwardRequest = http.request({
-        host: VIDEO_STORAGE_HOST,
-        port: VIDEO_STORAGE_PORT,
-        path: `/video?path=${videoRecord.videoPath}`,
-        method: 'GET',
-        headers: req.headers,
-      },
-      forwardResponse => {
-        const headers = forwardResponse.headers as IncomingHttpHeaders;
-        res.writeHead(forwardResponse.statusCode, headers);
-        forwardResponse.pipe(res);
+//       const forwardRequest = http.request({
+//         host: VIDEO_STORAGE_HOST,
+//         port: VIDEO_STORAGE_PORT,
+//         path: `/video?path=${videoRecord.videoPath}`,
+//         method: 'GET',
+//         headers: req.headers,
+//       },
+//       forwardResponse => {
+//         const headers = forwardResponse.headers as IncomingHttpHeaders;
+//         res.writeHead(forwardResponse.statusCode, headers);
+//         forwardResponse.pipe(res);
+//       });
+
+//       req.pipe(forwardRequest);
+//   });
+
+//   app.listen(PORT, () => console.log(`Video streaming microservice connected to port ${PORT}`));
+// };
+
+async function main() {
+
+  const app = express();
+
+  app.get("/video", async (req, res) => { // Route for streaming video.
+      
+      const videoPath = "./assets/videos/SampleVideo_1280x720_1mb.mp4";
+      const stats = await fs.promises.stat(videoPath);
+  
+      res.writeHead(200, {
+          "Content-Length": stats.size,
+          "Content-Type": "video/mp4",
       });
 
-      req.pipe(forwardRequest);
+      fs.createReadStream(videoPath).pipe(res);
   });
 
-  app.listen(PORT, () => console.log(`Video streaming microservice connected to port ${PORT}`));
-};
+  app.listen(PORT, () => {
+      console.log("Microservice online.");
+  });
+}
 
 main()
   .catch(err => {

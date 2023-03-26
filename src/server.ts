b4f185b47290d05b3b3cfb61872b3ccc19f0a1a3
zezang <zezang@gmail.com>
2023-03-26 15:23:03 -0700

    set up server file

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,18 @@ const PORT: number | string = process.env.PORT || 3000;
 const app: express.Express = express();
 
 app.get('/video', async(req: Request, res: Response) => {
+  const videoPath = './videos/SampleVideo_1280x720_1mb.mp4';
+  const stats = await fs.promises.stat(videoPath);
 
+  res.writeHead(200, {
+    'Content-Length': stats.size,
+    'Content-Type': 'video/mp4',
+  });
+
+  fs.createReadStream(videoPath).pipe(res);
 });
+
+app.listen(PORT, () => console.log('Video microservice listening on port: ', PORT));
 
 
 

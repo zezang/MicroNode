97ac5581ab768c4183e6a2e2fd0fd188dcd8f0e1
zezang <zezang@gmail.com>
2023-03-31 23:03:14 -0700

    added container registry tf file

---
+++ container-registry.tf
@@ -0,0 +1,20 @@
+resource "azurerm_container_registry" "container_registry" {
+    name                = "micronode"
+    resource_group_name = azurerm_resource_group.micronode.name
+    location            = "westus"
+    admin_enabled       = true
+    sku                 = "Basic"
+}
+
+output "registry_hostname" {
+    value = azurerm_container_registry.container_registry.login_server
+}
+
+output "registry_un" {
+    value =azurerm_container_registry.container_registry.admin_username
+}
+
+output "registry_pw" {
+    value = azurerm_container_registry.container_registry.admin_password
+    sensitive = true
+}
